7d2d0fe346b60691e9634e6971594ba9e53e4509
Aneesh0631 <101163524+Aneesh0631@users.noreply.github.com>
2022-04-06 12:36:01 +0530

    added Admin login
    Added login logic and end point for admin

--- src/main/java/com/Group1/Sprint/Controllers/AdminController.java
+++ src/main/java/com/Group1/Sprint/Controllers/AdminController.java
@@ -1,4 +1,45 @@
 package com.Group1.Sprint.Controllers;
 
+import com.Group1.Sprint.Exceptions.UserDetailsMissingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AdminController {
+    private String adminusername="admin";
+    private String adminpassword="admin123";
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> adminLogin(@RequestBody Map<String,String> userDetails)
+    {
+        Map<String, String> response = new HashMap<>();
+        try
+        {
+            if(userDetails.get("username")== null)
+            {
+                throw new UserDetailsMissingException("Please Enter Username");
+            }
+            if(userDetails.get("password")== null)
+            {
+                throw new UserDetailsMissingException("Please Enter Password");
+            }
+            if(userDetails.get("username").equals(adminusername) && userDetails.get("password").equals(adminpassword))
+            {
+                response.put("Status","Successful");
+                response.put("SessionID","1234");
+                return new ResponseEntity<Map<String, String>>(response, HttpStatus.OK);
+            }
+        }
+        catch(Exception e)
+        {
+            response.put("Status","Failed");
+            response.put("Error",e.getMessage());
+            return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Map<String, String>>(response, HttpStatus.NOT_FOUND);
+
+    }
 }
