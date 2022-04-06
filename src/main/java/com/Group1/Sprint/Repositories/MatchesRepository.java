cfdcb767e0ebe2d2dd24ee5975bd4846c0d844cd
harsha-krupashankar <harshasid510@gmail.com>
2022-04-06 12:19:59 +0530

    Added bids

--- src/main/java/com/Group1/Sprint/Repositories/MatchesRepository.java
+++ src/main/java/com/Group1/Sprint/Repositories/MatchesRepository.java
@@ -4,5 +4,6 @@ package com.Group1.Sprint.Repositories;
 import com.Group1.Sprint.Models.MatchesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface MatchesRepository extends JpaRepository<MatchesModel, Integer> {
 }
--- src/main/java/com/Group1/Sprint/Services/BidderService.java
+++ src/main/java/com/Group1/Sprint/Services/BidderService.java
@@ -1,7 +1,13 @@
 package com.Group1.Sprint.Services;
 
+import com.Group1.Sprint.Models.BidsModel;
+import com.Group1.Sprint.Models.MatchesModel;
+import com.Group1.Sprint.Models.TeamModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class BidderService<BidsRepository, TeamRepository, MatchesRepository, BidderRepository> implements IBidderService {
@@ -16,6 +22,29 @@ public class BidderService<BidsRepository, TeamRepository, MatchesRepository, Bi
 
     @Autowired
     BidsRepository bidsRepository;
+
+    @Override
+    public boolean bid(Map<String,Integer> bidDetails, int bidderId)
+    {
+        if(bidDetails.get("matchId")==null){throw new RuntimeException("Enter Match ID");}
+        if(bidDetails.get("teamId")==null){throw new RuntimeException("Enter Team ID");}
+
+        Optional<MatchesModel> match = matchesRepository.findById(bidDetails.get("matchId"));
+        Optional<TeamModel> team = teamRepository.findById(bidDetails.get("teamId"));
+
+        if(!match.isPresent()){ throw new RuntimeException("Match Not Present");}
+        if(!team.isPresent()){throw new RuntimeException("Team Not Present");}
+
+        MatchesModel m = match.get();
+        TeamModel t = team.get();
+        BidsModel bidsModel = new BidsModel();
+        bidsModel.setBidderId(bidderId);
+        bidsModel.setMatchId(m.getMatchId());
+        bidsModel.setTeamId(t.getTeamId());
+        bidsRepository.save(bidsModel);
+
+        return true;
+    }
 
 
 }
